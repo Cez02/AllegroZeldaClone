a9d3239afb722187dac3f56fde40287a16592431
Cez02 <czarek.czubala@gmail.com>
2022-01-31 20:29:34 +0100

    Fixed building for newer GCC

--- build-and-run.sh
+++ build-and-run.sh
@@ -16,7 +16,7 @@ mkdir build
 
 cp -r src/Assets build
 
-gcc ${lowlevel}Generics.c ${lowlevel}SoundHandler.c ${lowlevel}Physics.c ${lowlevel}InputHandler.c ${lowlevel}DisplayHandler.c ${menu}MainMenuHandler.c ${settings}SettingsHandler.c ${ingame}TerrainHandler.c ${ingame}PlayerHandler.c ${ingame}EnemyHandler.c ${ingame}UIHandler.c ${ingame}GameHandler.c src/main.c -o ./build/play $(pkg-config allegro-5 allegro_font-5 allegro_ttf-5 allegro_image-5 allegro_primitives-5 allegro_audio-5 allegro_acodec-5 --libs --cflags)
+gcc -fcommon ${lowlevel}Generics.c ${lowlevel}SoundHandler.c ${lowlevel}Physics.c ${lowlevel}InputHandler.c ${lowlevel}DisplayHandler.c ${menu}MainMenuHandler.c ${settings}SettingsHandler.c ${ingame}TerrainHandler.c ${ingame}PlayerHandler.c ${ingame}EnemyHandler.c ${ingame}UIHandler.c ${ingame}GameHandler.c src/main.c -o ./build/play $(pkg-config allegro-5 allegro_font-5 allegro_ttf-5 allegro_image-5 allegro_primitives-5 allegro_audio-5 allegro_acodec-5 --libs --cflags)
 
 cd build
 
